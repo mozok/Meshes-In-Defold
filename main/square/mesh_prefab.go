aea9fdf24cec08fe315806f04e9d9ccbc85f99dd
Mozok Evgen <mozokevgen@gmail.com>
2025-08-29 18:51:13 +0300

    Initial commit

---
+++ main/square/mesh_prefab.go
@@ -0,0 +1,9 @@
+embedded_components {
+  id: "mesh"
+  type: "mesh"
+  data: "material: \"/assets/materials/model_no_light.material\"\n"
+  "vertices: \"/main/square/empty.buffer\"\n"
+  "position_stream: \"position\"\n"
+  "normal_stream: \"position\"\n"
+  ""
+}
---
+++ main/square/square_base.go
@@ -0,0 +1,10 @@
+embedded_components {
+  id: "mesh"
+  type: "mesh"
+  data: "material: \"/assets/materials/model_no_light.material\"\n"
+  "vertices: \"/main/square/square_base.buffer\"\n"
+  "textures: \"/assets/images/green_8x8.png\"\n"
+  "position_stream: \"position\"\n"
+  "normal_stream: \"position\"\n"
+  ""
+}
